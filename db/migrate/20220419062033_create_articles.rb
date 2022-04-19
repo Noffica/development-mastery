449eb2a59ae61cf50e36554f47633d2cd614e9b5
Nauman Mithani <nmithani1@gmail.com>
2022-04-18 23:47:55 -0700

    Migration file edited

--- db/migrate/20220419062033_create_articles.rb
+++ db/migrate/20220419062033_create_articles.rb
@@ -1,8 +1,9 @@
 class CreateArticles < ActiveRecord::Migration[7.0]
   def change
     create_table :articles do |t|
-
-      t.timestamps
+      t.string      :title
+      t.text        :text #greater limit for length
+      t.timestamps        #`created_at` and `updated_at` will be added
     end
   end
 end
