bfd87ab07282ddb5aa87bfcf33efe90c614b8912
Nauman Mithani <nmithani1@gmail.com>
2022-06-22 23:01:39 -0700

    Setting up sample views
    Komments model and controller;
    
    Komment controller then model destroyed.
    
    index (all), single, new, create, params
    with Sherwin

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,30 @@
 class ArticlesController < ApplicationController
+  def hello
+    @message = "Hello there!"
+  end
+
+  # display all
+  def index
+    @articles_all = Article.all
+  end
+
+  # a new one
+  def new
+    @article = Article.new
+  end
+  # do NOT use #create as an alias; it is used for something
+
+  # load a single, particular Article object
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    a = Article.new(params[:article])
+    a.save
+
+    # look into params and related permissions: https://api.rubyonrails.org/classes/ActionController/Parameters.html
+    # look into #redirect_to and more explicit alternatives to loading the newly created Article object
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
+## Default
 Rails.application.routes.draw do
-  resources :widgets
+  resources :articles
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "articles#index"
 end
+
+# Rails.application.routes.draw do
+#   get 'articles/index'
+#   get 'articles/hello'
+#   get 'articles/new'
+# end
+
