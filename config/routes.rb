1b08ff6153649e1fb2eb223b8d67f65913e8dc98
Nauman Mithani <nmithani1@gmail.com>
2022-07-07 17:18:59 -0700

    index (all), single, new, create, params with Sherwin

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 ## Default
-# Rails.application.routes.draw do
-#   resources :widgets
-#   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-#   # Defines the root path route ("/")
-#   root "articles#index"
-# end
-
 Rails.application.routes.draw do
-  get 'articles/index'
-  get 'articles/hello'
+  resources :articles
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "articles#index"
 end
+
+# Rails.application.routes.draw do
+#   get 'articles/index'
+#   get 'articles/hello'
+#   get 'articles/new'
+# end
 
