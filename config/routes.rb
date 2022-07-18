3e50909d972b1e59995a5b7b2f8f180689713c67
Nauman Mithani <nmithani1@gmail.com>
2022-07-17 21:29:20 -0700

    Step back and retrace up to Rails guide 7.2 https://guides.rubyonrails.org/getting_started.html#resourceful-routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 ## Default
 Rails.application.routes.draw do
-  resources :articles
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   root "articles#index"
+
+  get "/articles",      to: "articles#index"
+  get "/articles/:id",  to: "articles#show"
+  
+  # resources :articles
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 end
 
 # Rails.application.routes.draw do
