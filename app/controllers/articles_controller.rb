e8a764f50aadd6f9c85c43dce88f1560e7276504
Nauman Mithani <nmithani1@gmail.com>
2022-07-21 19:24:59 -0700

    7.4.1 done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,7 +11,7 @@ class ArticlesController < ApplicationController
 
   # a new article is **only** instantiated, not saved
   def new
-    @article_new = Article.new
+    @article = Article.new
   end
   # do NOT use #create as an alias; it is used for something
 
@@ -22,17 +22,33 @@ class ArticlesController < ApplicationController
 
   # create a single, new instance of Article
   def create
-    @article_new = Article.new(article_params)
+    @article = Article.new(article_params)
     
-    if @article_new.save
-      redirect_to(@article_new)
+    if @article.save
+      redirect_to(@article)
     else
       render(:new, status: :unprocessable_entity)
     end
-
-    #TODO look into params and related permissions: https://api.rubyonrails.org/classes/ActionController/Parameters.html
-    #TODO look into #redirect_to and more explicit alternatives to loading the newly created Article object
   end
+
+  # edit a single, particular instance of Article
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to(@article)
+    else
+      render(:edit, status: :unprocessable_entity)
+    end
+  end
+
+
+  #TODO look into params and related permissions: https://api.rubyonrails.org/classes/ActionController/Parameters.html
+  #TODO look into #redirect_to and more explicit alternatives to loading the newly created Article object
 
   private
   def article_params
