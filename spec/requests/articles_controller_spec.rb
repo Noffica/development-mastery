1b3b23063090062eab14a04beafc3e31c17c05e1
Nauman Mithani <nmithani1@gmail.com>
2022-09-15 19:43:43 -0700

    L10: shared examples

--- spec/requests/articles_controller_spec.rb
+++ spec/requests/articles_controller_spec.rb
@@ -1,31 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe "Article", type: :request do
-  # let(:valid_attributes) { title: "Abcd", body: "Efgh" }
+  RSpec.shared_examples 'successful response' do
+    it 'has successful response' do
+      expect(response).to(be_successful)
+      expect(response).to(have_http_status(:ok))
+    end
+  end
 
   describe "GET #index" do
     before(:each) do
       get(articles_path)
     end
     
-    it 'has successful response' do
-      expect(response).to(be_successful)
-      expect(response).to(have_http_status(:ok))
-    end
-    it 'renders its template' do
+    include_examples "successful response"
+
+    it "renders its template" do
       expect(response).to(render_template(:index))
     end
   end #describe
 
   describe "GET #show" do
     let(:article_one) { Article.create(title: 'A', body: 'abcd') }
+    
     before(:each) do
       get(article_path(article_one))
     end
     
-    it "has a successful response" do
-      expect(response).to(be_successful)
-    end
+    include_examples "successful response"
+
     it "renders its template" do
       expect(response).to(render_template(:show))
     end
