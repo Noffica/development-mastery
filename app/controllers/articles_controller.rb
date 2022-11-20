629bdc0e69511f81a6b4609a0993da2236bdee0c
Nauman Mithani <nmithani1@gmail.com>
2022-11-20 15:20:17 -0800

    10: All tests pass; DELETE remains

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   
-  before_action :set_article, only: [:show, :edit, :create, :update, :destroy]
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   def hello
     @message = "Hello there!"
@@ -28,7 +28,7 @@ class ArticlesController < ApplicationController
 
   # create a single, new instance of Article
   def create
-    # before_action
+    @article = Article.new(article_params)
     
     if @article.save
       redirect_to(@article)
