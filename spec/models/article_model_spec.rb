c8745fc3ceb6163651129a506849b3eded846534
Nauman Mithani <nmithani1@gmail.com>
2023-03-07 18:46:56 -0800

    Use of Factory. Clean-up complete. Ready for PR.

--- spec/models/article_model_spec.rb
+++ spec/models/article_model_spec.rb
@@ -3,21 +3,21 @@ require 'rails_helper'
 # #build*, #create are from FactoryBot
 
 RSpec.describe Article, type: :model do
-  let(:subject) { described_class.new }
-
-  let(:article_two) { Article.new }
-
   context "when considering all data attributes" do
     it "is invalid without required parameters" do
-      # expect(subject).to be_invalid
-      expect(build(:article, :no_attributes)).to(be_invalid)
+      expect(
+        build(:article, :no_attributes)
+      ).to(
+        be_invalid
+      )
     end
 
     it "is valid with all of its parameters" do
-      # subject.title = "A"
-      # subject.body  = "abcd"
-    
-      expect(build(:article, :valid_attributes)).to(be_valid)
+      expect(
+        build(:article, :valid_attributes)
+      ).to(
+        be_valid
+      )
     end
 
     pending "is valid with required parameters, where optional parameters are omitted"
@@ -33,36 +33,17 @@ RSpec.describe Article, type: :model do
       ).to(
         include("has already been taken")
       )
-
-      # subject.title = "A"
-      # subject.body  = "abcd"
-      # subject.save
-
-      # article_two.title = subject.title
-      # article_two.body  = subject.body
-      
-      # article_two.valid?
-      # expect(article_two.errors.full_messages.first).to(include("has already been taken"))
     end
   end #context
 
   context "when checking the *Title* attribute and fixing the Body attribute" do
-    # before(:each) do
-      # subject.body = 'abcd'
-    # end
-
-    let(:article_one) { build(:article, body: 'abcd') }
-
     context "include `presence: true` guard" do
       it 'is rejected if `Title` is `nil`' do
-        # expect(article_one).to(be_invalid)
         expect(
           build(:article, :body_attribute_only)
         ).to(
           be_invalid
         )
-        # subject.title = nil
-        # expect(subject).to(be_invalid)
       end
 
       it 'is rejected if `Title` is empty/blank' do
@@ -71,12 +52,6 @@ RSpec.describe Article, type: :model do
         ).to(
           be_invalid
         )
-        
-        # article_one.title = ''
-        # expect(article_one).to(be_invalid)
-
-        # subject.title = ''
-        # expect(subject).to(be_invalid)
       end
 
       it 'is rejected if `Title` is only spaces' do
@@ -85,9 +60,6 @@ RSpec.describe Article, type: :model do
         ).to(
           be_invalid
         )
-
-        # subject.title = ' '
-        # expect(subject).to(be_invalid)
       end
     end #context
 
@@ -98,10 +70,6 @@ RSpec.describe Article, type: :model do
   end #context
 
   context "when checking the *Body* attribute and fixing the Title attribute" do
-    before(:each) do
-      subject.title = "A"
-    end
-
     pending "satisfies max. boundary (length)"
     pending "fails max. + 1 boundary (length)"
  
@@ -112,9 +80,6 @@ RSpec.describe Article, type: :model do
         ).to(
           be_invalid
         )
-        
-        # subject.body = nil
-        # expect(subject).to(be_invalid)
       end
       
       it 'is rejected if `Body` is empty/blank' do
@@ -123,18 +88,14 @@ RSpec.describe Article, type: :model do
         ).to(
           be_invalid
         )
-        # subject.body = ''
-        # expect(subject).to(be_invalid)
       end
 
-      it 'is rejected if value of `Body` is only spaces' do
+      it 'is rejected if `Body` is only spaces' do
         expect(
           build(:article, :title_attribute_only, body: ' ')
         ).to(
           be_invalid
         )
-        # subject.body = ' '
-        # expect(subject).to(be_invalid)
       end
     end #context
 
